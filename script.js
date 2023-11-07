4703bdc6ea8579404797f3c84e369ae1e2c96ab3
LENI4C <timibraperetei74@gmail.com>
2023-11-07 13:06:15 +0100

    changing views

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-const box = document.querySelectorAll(".box")
+// ## VIEW 1 ##
+const viewOne = document.querySelector(".sec-one")
+const vsCompBtn = document.querySelector(".vsComp")
+const vsHumBtn = document.querySelector(".vsHum")
+let userVs = null
+
+// ## VIEW 2 ##
+const viewTwo = document.querySelector(".sec-two")
 const userChoiceX = document.querySelector(".x")
 const userChoiceO = document.querySelector(".o")
-const board = document.querySelector(".board")
 let userChoice = null
 
-userChoiceX.addEventListener("click", () => {
-    userChoice = "❌"
-})
-
-userChoiceO.addEventListener("click", () => {
-    userChoice = "⭕"
-})
+// ## VIEW 3 ##
+const viewThree = document.querySelector(".sec-three")
+const box = document.querySelectorAll(".box")
+const board = document.querySelector(".board")
 
 
-box.addEventListener("click", (e) => {
-    e.target.textContent = userChoice
-})
+
+if (userVs == null) {
+            
+    vsCompBtn.addEventListener("click", () => {
+        userVs = vsCompBtn
+        alert(`you have chosen to${userVs.textContent.toLowerCase()}`)
+        viewTwo.style.display = "flex"
+        viewOne.style.display = "none"
+    })
+
+    vsHumBtn.addEventListener("click", () => {
+        userVs = vsHumBtn
+        alert(`you have chosen to${userVs.textContent.toLowerCase()}`)
+        viewTwo.style.display = "flex"
+        viewOne.style.display = "none"
+    })
+
+    userChoiceX.addEventListener("click", () => {
+        userChoice = "❌"
+        alert(`you are${userChoice}`)
+        viewTwo.style.display = "none"
+        viewOne.style.display = "none"
+        viewThree.style.display = "flex"
+    })
+    
+    userChoiceO.addEventListener("click", () => {
+        userChoice = "⭕"
+        alert(`you are${userChoice}`)
+        viewTwo.style.display = "none"
+        viewOne.style.display = "none"
+        viewThree.style.display = "flex"
+    })
+} 
